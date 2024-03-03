dc86e4d2f2d85314a0fd67befad8459df356e057
codemasteryacademy <codemasteryacademy@gmail.com>
2024-03-03 21:48:56 +0000

    Update script.js

--- styles/animations/script.js
+++ styles/animations/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const myObserver = new IntersectionObserver(function (entries) {
-    console.log(entries);
     entries.forEach(entry => {
         if(entry.isIntersecting && entry.target.classList.contains('hidden')) {
             entry.target.classList.add('show');
